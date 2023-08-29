9c20f8dc0165de592b13a0fea24a92ce48e5b34a
tokiakasu <>
2023-08-29 22:27:53 +0300

    Add fade-in effect via jQuery

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,16 +3,3 @@
 particlesJS.load('particles-js', 'assets/particles.json', function() {
     // console.log('particles.js loaded - callback');
 });
-
-
-// from https://verstaem.online/blog/fadein-i-fadeout-na-chistom-js/
-
-const fadeIn = (el, timeout, display) => {
-  el.style.opacity = 0;
-  el.style.display = display || 'particles-js';
-  el.style.transition = `opacity ${timeout}ms`;
-  setTimeout(() => {
-    el.style.opacity = 1;
-  }, 10);
-};
-
